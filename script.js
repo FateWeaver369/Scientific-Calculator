9c5ec91469aeb66aa3c74e85680db143e46a4495
FateWeaver369 <greyfateweaver@gmail.com>
2026-03-19 22:50:46 +0530

    Enhance calculator with sound and history features
    Refactor calculator functions and add sound effects

--- script.js
+++ script.js
@@ -1,31 +1,71 @@
-let display = document.getElementById("display");
+let screen = document.getElementById("screen");
+let historyDiv = document.getElementById("history");
+let angleMode = "DEG";
 
-function press(num){
-display.value += num;
+const sound = new Audio("sound.mp3");
+
+function playSound(){
+  sound.currentTime = 0;
+  sound.play();
+}
+
+function append(val){
+  playSound();
+  screen.value += val;
+}
+
+function clearAll(){
+  playSound();
+  screen.value = "";
+}
+
+function del(){
+  playSound();
+  screen.value = screen.value.slice(0,-1);
+}
+
+function toggleAngle(){
+  angleMode = angleMode === "DEG" ? "RAD" : "DEG";
+  event.target.innerText = angleMode;
+}
+
+function func(type){
+  playSound();
+  screen.value += type + "(";
 }
 
 function calculate(){
-display.value = eval(display.value);
+  try{
+    let expr = screen.value;
+
+    if(angleMode === "DEG"){
+      expr = expr.replace(/sin\((.*?)\)/g, (_,x)=>`Math.sin(${x} * Math.PI/180)`);
+      expr = expr.replace(/cos\((.*?)\)/g, (_,x)=>`Math.cos(${x} * Math.PI/180)`);
+      expr = expr.replace(/tan\((.*?)\)/g, (_,x)=>`Math.tan(${x} * Math.PI/180)`);
+    } else {
+      expr = expr.replace(/sin/g,"Math.sin")
+                 .replace(/cos/g,"Math.cos")
+                 .replace(/tan/g,"Math.tan");
+    }
+
+    let result = eval(expr);
+
+    addHistory(screen.value, result);
+
+    screen.value = result;
+
+  } catch {
+    screen.value = "Error";
+  }
 }
 
-function clearDisplay(){
-display.value = "";
+function addHistory(exp, res){
+  let div = document.createElement("div");
+  div.textContent = `${exp} = ${res}`;
+  historyDiv.prepend(div);
 }
 
-function scientific(func){
-
-let value = parseFloat(display.value);
-
-if(func === "sin"){
-display.value = Math.sin(value);
-}
-
-if(func === "cos"){
-display.value = Math.cos(value);
-}
-
-if(func === "tan"){
-display.value = Math.tan(value);
-}
-
-}
+/* THEME SWITCH */
+document.getElementById("theme").addEventListener("change", (e)=>{
+  document.body.className = e.target.value;
+});
